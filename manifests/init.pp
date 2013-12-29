6f4a87e4826693d127065bfc2104dc349be0adb2
Tim Chan <tim@chan.net>
2013-12-28 23:13:16 -0800

    ready to go

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,17 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Cisco AnyConnect to /Applications
+#
+# Sample Usage:
+#
+#  class { 'cisco_anyconnect' :
+#    version => '3.1.04063'
+#  }
+#
+
+
+class cisco_anyconnect($version='3.1.04063')
+{
+  package { "anyconnect-macosx-${version}-web-deploy-k9.dmg":
+    provider => 'pkgdmg',
+    source   => "http://turbo.edmunds.com/CACHE/stc/2/binaries/anyconnect-macosx-${version}-web-deploy-k9.dmg"
+  }
 }
